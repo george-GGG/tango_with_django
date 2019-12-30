84d818bff76c0e6e71498599c1bc2929cb18f108
zhiguo <chenzhiguoggg@qq.com>
2019-12-30 20:42:25 +0800

    form-the last work before deploy

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
+from django.urls import reverse
+
 def index(request): 
 	#return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>about</a>")
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -49,4 +54,50 @@ def show_category(request,category_name_slug):
 	# Go render the response and return it to the client. 
 	return render(request,'rango/category.html',context_dict)
 
+def add_category(request):
+	form = CategoryForm()
 
+	#A HTTP POST? 
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		# Have we been provided with a valid form? 
+		if form.is_valid():
+			# Save the new category to the database. 
+			form.save(commit=True)
+			# Now that the category is saved 
+			# # We could give a confirmation message 
+			# # But since the most recent category added is on the index page 
+			# # Then we can direct the user back to the index page.
+			return index(request)
+		else:
+			# The supplied form contained errors 
+			# # just print them to the terminal. 
+			print(form.errors)
+	# Will handle the bad form, new form, or no form supplied cases. 
+	# # Render the form with error messages (if any). 
+	return render(request,'rango/add_category.html',{'form':form})
+
+def add_page(request,category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+
+				return redirect(reverse('rango:show_category',kwargs={'category_name_slug':category_name_slug}))
+		else:
+			print(form.errors)
+		
+	context_dict = {'form':form,'category':category}
+	#ap_url="rango/category/"+category_name_slug+"/add_page/"
+	return render(request, 'rango/add_page.html' ,context_dict)#view是服务器后台操作，不需要url映射，直接给真实路径。
